ee18a953c6ab4fc890651eb2d7c1eeac412575c8
Renan17px <renanegrini110@gmail.com>
2026-09-02 11:22:22 -0300

    Delete src directory

--- src/br/com/cyberfestival/model/IngressoInvalido.java
+++
@@ -1,7 +0,0 @@
-package br.com.cyberfestival.model;
-
-public class IngressoInvalido extends RuntimeException {
-    public IngressoInvalido() {
-        super("Erro de segurança: ingresso duplicado ou código já validado!");
-    }
-}
